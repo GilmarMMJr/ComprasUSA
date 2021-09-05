c91ac9d5a6aed80e8d353f7fcf01073798b183d4
GilmarMMJr <gilmar_mendonca@live.com>
2021-09-05 19:46:52 -0300

    implemented screen elements

---
+++ ComprasUSA/ComprasUSA/TaxesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaxesViewController.swift
+//  ComprasUSA
+//
+//  Created by Gilmar Junior on 05/09/21.
+//
+
+import UIKit
+
+class TaxesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
